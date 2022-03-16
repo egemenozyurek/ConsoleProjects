6895c20091a7f43387e0c34d3a51c496a2a18f3b
egemenozyurek <ozyurekegemen95@gmail.com>
2022-03-16 18:39:14 +0300

    conditional statements

---
+++ ConditionalStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace conditionalstatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("İyi Günler");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler");
+            }
+
+            string result = time <= 18 ? "İyi günler" : "İyi Geceler";
+
+            result = time >= 6 && time < 11 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
