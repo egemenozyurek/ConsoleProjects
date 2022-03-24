290c99e187379bccf015b3eb8c25c68e5ddb3852
egemenozyurek <ozyurekegemen95@gmail.com>
2022-03-24 15:56:45 +0300

    collections sample

---
+++ AlgorithQuestionsWithCollections/Q3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Q3 {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Bir cümle giriniz:");
+            string input = Console.ReadLine ();
+            char[] sesliHarfler = { 'a', 'e', 'i', 'o', 'u' };
+
+            List<char> harfler = new List<char> ();
+
+            foreach (var item in input) {
+                foreach (var sabit in sesliHarfler) {
+                    if (item == sabit)
+                        harfler.Add (item);
+                }
+            }
+
+            Console.WriteLine ("Cümle içerisindeki sesli harfler :");
+            foreach (var item in harfler) {
+                Console.Write (item + " ");
+            }
+        }
+    }
+}
